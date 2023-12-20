0551c7eba20f0a86703b59ec6131259a753bb996
Meenakshi <Meenakshi@shill>
2023-12-20 20:16:17 +0530

    initial commit

---
+++ src/main/java/com/example/VenturingDigitally/VenturingDigitallyApplication.java
@@ -0,0 +1,13 @@
+package com.example.VenturingDigitally;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenturingDigitallyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenturingDigitallyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VenturingDigitally/VenturingDigitallyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VenturingDigitally;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenturingDigitallyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
